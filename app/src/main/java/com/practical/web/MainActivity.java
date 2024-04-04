f5ab999f221b34a18f3ff73438469a58c78d27b6
Ibram <shaikhibuu07@gmail.com>
2024-04-05 00:17:23 +0530

    Initial commit

---
+++ app/src/main/java/com/practical/web/MainActivity.java
@@ -0,0 +1,24 @@
+package com.practical.web;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript support
+
+        // Load a webpage
+        webView.loadUrl("https://www.google.com");
+    }
+}
